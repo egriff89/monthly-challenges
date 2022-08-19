4f04f1b3600fb1b94dd2fc74546eefcfde2e82e4
Eric Griffith <eric.griffith42@gmail.com>
2022-08-19 02:12:46 -0500

    Refactor views to work with new index template

--- challenges/views.py
+++ challenges/views.py
@@ -15,20 +15,16 @@ monthly_challenges = {
    'september': 'Learn Django for at least 20 minutes every day!',
    'october': 'Eat no meat for the entire month!',
    'november': 'Walk at least 20 minutes every day!',
-   'december': 'Learn Django for at least 20 minutes every day!',
+   'december': None,
 }
 
 
 def index(request):
-   list_items = ''
    months = list(monthly_challenges.keys())
 
-   for month in months:
-      month_path = reverse('month-challenge', args=[month])
-      list_items += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
-
-   response_data = f'<ul>{list_items}</ul>'
-   return HttpResponse(response_data)
+   return render(request, 'challenges/index.html', {
+      'months': months
+   })
 
 
 def monthly_challenge_by_number(request, month):
